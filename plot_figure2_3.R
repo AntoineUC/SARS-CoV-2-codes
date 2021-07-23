78c4d7d54f695b452e39683b270b5ab8608dd085
AntoineUC <56665669+AntoineUC@users.noreply.github.com>
2021-07-23 15:32:07 +0200

    Add files via upload

---
+++ plot_figure2_3.R
@@ -0,0 +1,623 @@
+rm(list=ls())
+
+library(maps)
+
+library(mapdata)
+
+par(mfrow=c(1,3))
+
+country=read.table(file="country cluster.txt",sep="\n")
+
+country=country[,1]
+
+sum(country=="Italy")
+
+usindex=which(country=="Italy")
+
+total.cases=read.table(file="total cases.txt",header=F,sep="\n")
+
+total.cases=total.cases[,1]
+
+uscases=total.cases[usindex]
+
+uscases=as.numeric(as.vector(uscases))
+
+Z=uscases
+
+ddgpd=function(k,igamma,sigma){
+  return((1+igamma*k/sigma)^(-1/igamma)-(1+igamma*(k+1)/sigma)^(-1/igamma))
+}
+
+n=length(Z)
+
+Ztot=Z
+
+########################################
+
+vecu=unique(sort(Z))
+
+meanexcess=rep(0,length(vecu))
+
+i=1
+
+while(i<=length(vecu)){
+  
+  meanexcess[i]=mean(Z[which(Z>=vecu[i])]-vecu[i])
+  
+  i=i+1
+}
+
+###################################
+
+llfunction=function(theta){
+  igamma=theta[1]
+  sigma=theta[2]
+  return(-sum(log((ddgpd(Z,igamma,sigma)))))
+}
+
+llfunction2=function(igamma,sigma){
+  return(-sum(log(((1+igamma*Z/sigma)^(-1/igamma)-(1+igamma*(Z+1)/sigma)^(-1/igamma)))))
+}
+
+dllfunction2gamma=function(igamma,sigma){
+  return((llfunction2(igamma+0.001,sigma)-llfunction2(igamma,sigma))/0.001)
+}
+
+dllfunction2gammasigma=function(igamma,sigma){
+  return((dllfunction2gamma(igamma,sigma+0.001)-dllfunction2gamma(igamma,sigma))/0.001)
+}
+
+dllfunction2gamma2=function(igamma,sigma){
+  return((dllfunction2gamma(igamma+0.001,sigma)-dllfunction2gamma(igamma,sigma))/0.001)
+}
+
+dllfunction2sigma=function(igamma,sigma){
+  return((llfunction2(igamma,sigma+0.001)-llfunction2(igamma,sigma))/0.001)
+}
+
+dllfunction2sigma2=function(igamma,sigma){
+  return((dllfunction2sigma(igamma,sigma+0.001)-dllfunction2sigma(igamma,sigma))/0.001)
+}
+
+k=1
+
+#vecu=1:20
+
+vecu=unique(sort(Ztot))
+
+vecgamma=rep(0,length(vecu))
+
+vecgammainf=rep(0,length(vecu))
+
+vecgammasup=rep(0,length(vecu))
+
+vecsigma=rep(0,length(vecu))
+
+vecsigmainf=rep(0,length(vecu))
+
+vecsigmasup=rep(0,length(vecu))
+
+while(k<=length(vecu)){
+  
+  Z=Ztot[which(Ztot>=vecu[k])]-vecu[k]
+  
+  nk=length(Z)
+  
+  thetahat=optim(fn=llfunction,par=c(0.25,0.5))$par
+  
+  vecgamma[k]=thetahat[1]
+  
+  vecsigma[k]=thetahat[2]
+  
+  I11=dllfunction2gamma2(thetahat[1],thetahat[2])
+  
+  I12=dllfunction2gammasigma(thetahat[1],thetahat[2])
+  
+  I21=I12
+  
+  I22=dllfunction2sigma2(thetahat[1],thetahat[2])
+  
+  matI=matrix(c(I11,I12,I21,I22),2,2)
+  
+  varhat=solve(matI)[1,1]
+  
+  varhatsigma=solve(matI)[2,2]
+  
+  vecgammasup[k]=vecgamma[k]+sqrt(varhat)*qnorm(0.95)
+  
+  vecgammainf[k]=vecgamma[k]+sqrt(varhat)*qnorm(0.05)
+  
+  vecsigmasup[k]=vecsigma[k]+sqrt(varhatsigma)*qnorm(0.95)
+  
+  vecsigmainf[k]=vecsigma[k]+sqrt(varhatsigma)*qnorm(0.05)
+  
+  k=k+1
+  
+}
+
+vecgamma
+
+vecgammacovid=vecgamma
+
+vecgammainfcovid=vecgammainf
+
+vecgammasupcovid=vecgammasup
+
+vecsigmacovid=vecsigma
+
+vecsigmasupcovid=vecsigmasup
+
+vecsigmainfcovid=vecsigmainf
+
+vecucovid=vecu
+
+plot(vecgammacovid~vecucovid,type="l",xlab="Threshold",ylab="xi",main="SARS-CoV-2 clusters in Italy",ylim=c(-1,3),xlim=c(1,100),col="red")
+
+points(vecgammainfcovid~vecucovid,type="l",lty=3,col="red")
+
+points(vecgammasupcovid~vecucovid,type="l",lty=3,col="red")
+
+abline(a=1,b=0,col="red",lty=5)
+
+abline(v=34,col="blue",lty=5)
+
+tau=0.99
+
+vecquant=1-(1+vecgamma[17]*((1:1000)+1)/vecsigma[17])^(-1/vecgamma[17])
+
+quantau=25+which(vecquant>=tau)[1]
+
+quantau
+
+
+###############################
+
+n=length(Ztot)
+
+vechill=rep(0,n-1)
+
+vecutot=rep(0,n-1)
+
+k=2
+
+while(k<=n){
+  
+  vecutot[k-1]=rev(sort(Ztot))[k]
+  
+  vechill[k-1]=mean(log(Ztot[which(Ztot>=vecutot[k-1])]/vecutot[k-1]))
+  
+  k=k+1
+}
+
+vecgammahill=unique(rev(vechill))
+
+vecuhill=unique(rev(vecutot))
+
+rm(list=ls())
+
+library(maps)
+
+library(mapdata)
+
+Z=c(580,68,39,275,46,61,40,38,73,64,134,21,1,1,38,50,9,26,30,197,57,12,22,113,26,2,7,19,68,39,46,33,87,85,39,24,19,49,55,31,48,132,63,27,17,54,46,12,13,22,9,44,36,49,47,61,17,12,23,12,4,13,13,14,13,4,16,6,6,6,17,11,66,15,4,3,3,2,2,2,13,18,33,17,31,20,18,15,38,7,13,21,3,18,22,24,84,39,22,20,18,67,66,27,18,34,41,17,20,29,35,17,12)
+
+uscases=Z
+
+uscases=as.numeric(as.vector(uscases[,1]))
+
+Z=uscases
+
+ddgpd=function(k,igamma,sigma){
+  return((1+igamma*k/sigma)^(-1/igamma)-(1+igamma*(k+1)/sigma)^(-1/igamma))
+}
+
+n=length(Z)
+
+Ztot=Z
+
+########################################
+
+vecu=unique(sort(Z))
+
+meanexcess=rep(0,length(vecu))
+
+i=1
+
+while(i<=length(vecu)){
+  
+  meanexcess[i]=mean(Z[which(Z>=vecu[i])]-vecu[i])
+  
+  i=i+1
+}
+
+###################################
+
+llfunction=function(theta){
+  igamma=theta[1]
+  sigma=theta[2]
+  return(-sum(log((ddgpd(Z,igamma,sigma)))))
+}
+
+llfunction2=function(igamma,sigma){
+  return(-sum(log(((1+igamma*Z/sigma)^(-1/igamma)-(1+igamma*(Z+1)/sigma)^(-1/igamma)))))
+}
+
+dllfunction2gamma=function(igamma,sigma){
+  return((llfunction2(igamma+0.001,sigma)-llfunction2(igamma,sigma))/0.001)
+}
+
+dllfunction2gammasigma=function(igamma,sigma){
+  return((dllfunction2gamma(igamma,sigma+0.001)-dllfunction2gamma(igamma,sigma))/0.001)
+}
+
+dllfunction2gamma2=function(igamma,sigma){
+  return((dllfunction2gamma(igamma+0.001,sigma)-dllfunction2gamma(igamma,sigma))/0.001)
+}
+
+dllfunction2sigma=function(igamma,sigma){
+  return((llfunction2(igamma,sigma+0.001)-llfunction2(igamma,sigma))/0.001)
+}
+
+dllfunction2sigma2=function(igamma,sigma){
+  return((dllfunction2sigma(igamma,sigma+0.001)-dllfunction2sigma(igamma,sigma))/0.001)
+}
+
+k=1
+
+#vecu=1:20
+
+vecu=unique(sort(Ztot))
+
+vecgamma=rep(0,length(vecu))
+
+vecgammainf=rep(0,length(vecu))
+
+vecgammasup=rep(0,length(vecu))
+
+vecsigma=rep(0,length(vecu))
+
+vecsigmainf=rep(0,length(vecu))
+
+vecsigmasup=rep(0,length(vecu))
+
+while(k<=length(vecu)){
+  
+  Z=Ztot[which(Ztot>=vecu[k])]-vecu[k]
+  
+  nk=length(Z)
+  
+  thetahat=optim(fn=llfunction,par=c(0.25,0.5))$par
+  
+  vecgamma[k]=thetahat[1]
+  
+  vecsigma[k]=thetahat[2]
+  
+  I11=dllfunction2gamma2(thetahat[1],thetahat[2])
+  
+  I12=dllfunction2gammasigma(thetahat[1],thetahat[2])
+  
+  I21=I12
+  
+  I22=dllfunction2sigma2(thetahat[1],thetahat[2])
+  
+  matI=matrix(c(I11,I12,I21,I22),2,2)
+  
+  varhat=solve(matI)[1,1]
+  
+  varhatsigma=solve(matI)[2,2]
+  
+  vecgammasup[k]=vecgamma[k]+sqrt(varhat)*qnorm(0.95)
+  
+  vecgammainf[k]=vecgamma[k]+sqrt(varhat)*qnorm(0.05)
+  
+  vecsigmasup[k]=vecsigma[k]+sqrt(varhatsigma)*qnorm(0.95)
+  
+  vecsigmainf[k]=vecsigma[k]+sqrt(varhatsigma)*qnorm(0.05)
+  
+  k=k+1
+  
+}
+
+vecgamma
+
+vecgammacovid=vecgamma
+
+vecgammainfcovid=vecgammainf
+
+vecgammasupcovid=vecgammasup
+
+vecsigmacovid=vecsigma
+
+vecsigmasupcovid=vecsigmasup
+
+vecsigmainfcovid=vecsigmainf
+
+vecucovid=vecu
+
+plot(vecgammacovid~vecucovid,type="l",xlab="Threshold",ylab="xi",main="SARS-CoV-2 clusters in Kerala",ylim=c(-1,3),xlim=c(1,100),col="red")
+
+points(vecgammainfcovid~vecucovid,type="l",lty=3,col="red")
+
+points(vecgammasupcovid~vecucovid,type="l",lty=3,col="red")
+
+abline(a=0.36,b=0,col="red",lty=5)
+
+abline(v=22,col="blue",lty=5)
+
+tau=0.99
+
+vecquant=1-(1+vecgamma[17]*((1:1000)+1)/vecsigma[17])^(-1/vecgamma[17])
+
+quantau=25+which(vecquant>=tau)[1]
+
+quantau
+
+
+###############################
+
+n=length(Ztot)
+
+vechill=rep(0,n-1)
+
+vecutot=rep(0,n-1)
+
+k=2
+
+while(k<=n){
+  
+  vecutot[k-1]=rev(sort(Ztot))[k]
+  
+  vechill[k-1]=mean(log(Ztot[which(Ztot>=vecutot[k-1])]/vecutot[k-1]))
+  
+  k=k+1
+}
+
+vecgammahill=unique(rev(vechill))
+
+vecuhill=unique(rev(vecutot))
+
+##########################################
+
+
+rm(list=ls())
+
+library(maps)
+
+library(evt0)
+
+library(Expectrem)
+
+country=read.table(file="country cluster.txt",sep="\n")
+
+country=country[,1]
+
+sum(country=="United States")
+
+usindex=which(country=="United States")
+
+total.cases=read.table(file="total cases.txt",header=F,sep="\n")
+
+total.cases=total.cases[,1]
+
+uscases=total.cases[usindex]
+
+uscases=as.numeric(as.vector(uscases))
+
+latitude=read.table(file="latitude clusters.txt",header=F,dec=".",sep="\n")
+
+latitude=latitude[,1]
+
+latitude=latitude[usindex]
+
+longitude=read.table(file="longitude clusters.txt",header=F,dec=".")
+
+longitude=longitude[,1]
+
+longitude=longitude[usindex]
+
+#points(longitude,latitude,cex=0.6,col="red")
+
+Y=uscases
+
+####################################
+
+californiaindex=(latitude<=41.21)*(latitude>38.55)*(longitude<=-73.53)*(longitude>-75.35)
+
+Ynj=Y[which(californiaindex==1)]
+
+points(longitude[which(californiaindex==1)],latitude[which(californiaindex==1)],cex=5*Ynj/max(Ynj),col="red")
+
+n=length(Ynj)
+
+####################################
+
+#################discrete GPD###############
+
+Z=Ynj
+
+ddgpd=function(k,igamma,sigma){
+  return((1+igamma*k/sigma)^(-1/igamma)-(1+igamma*(k+1)/sigma)^(-1/igamma))
+}
+
+n=length(Z)
+
+Ztot=Z
+
+########################################
+
+vecu=unique(sort(Z))
+
+meanexcess=rep(0,length(vecu))
+
+i=1
+
+while(i<=length(vecu)){
+  
+  meanexcess[i]=mean(Z[which(Z>=vecu[i])]-vecu[i])
+  
+  i=i+1
+}
+
+###################################
+
+llfunction=function(theta){
+  igamma=theta[1]
+  sigma=theta[2]
+  return(-sum(log((ddgpd(Z,igamma,sigma)))))
+}
+
+llfunction2=function(igamma,sigma){
+  return(-sum(log(((1+igamma*Z/sigma)^(-1/igamma)-(1+igamma*(Z+1)/sigma)^(-1/igamma)))))
+}
+
+dllfunction2gamma=function(igamma,sigma){
+  return((llfunction2(igamma+0.001,sigma)-llfunction2(igamma,sigma))/0.001)
+}
+
+dllfunction2gammasigma=function(igamma,sigma){
+  return((dllfunction2gamma(igamma,sigma+0.001)-dllfunction2gamma(igamma,sigma))/0.001)
+}
+
+dllfunction2gamma2=function(igamma,sigma){
+  return((dllfunction2gamma(igamma+0.001,sigma)-dllfunction2gamma(igamma,sigma))/0.001)
+}
+
+dllfunction2sigma=function(igamma,sigma){
+  return((llfunction2(igamma,sigma+0.001)-llfunction2(igamma,sigma))/0.001)
+}
+
+dllfunction2sigma2=function(igamma,sigma){
+  return((dllfunction2sigma(igamma,sigma+0.001)-dllfunction2sigma(igamma,sigma))/0.001)
+}
+
+k=1
+
+#vecu=1:20
+
+vecu=unique(sort(Ztot))
+
+vecgamma=rep(0,length(vecu))
+
+vecgammainf=rep(0,length(vecu))
+
+vecgammasup=rep(0,length(vecu))
+
+vecsigma=rep(0,length(vecu))
+
+vecsigmainf=rep(0,length(vecu))
+
+vecsigmasup=rep(0,length(vecu))
+
+while(k<=length(vecu)){
+  
+  Z=Ztot[which(Ztot>=vecu[k])]-vecu[k]
+  
+  nk=length(Z)
+  
+  thetahat=optim(fn=llfunction,par=c(0.25,0.5))$par
+  
+  vecgamma[k]=thetahat[1]
+  
+  vecsigma[k]=thetahat[2]
+  
+  I11=dllfunction2gamma2(thetahat[1],thetahat[2])
+  
+  I12=dllfunction2gammasigma(thetahat[1],thetahat[2])
+  
+  I21=I12
+  
+  I22=dllfunction2sigma2(thetahat[1],thetahat[2])
+  
+  matI=matrix(c(I11,I12,I21,I22),2,2)
+  
+  varhat=solve(matI)[1,1]
+  
+  varhatsigma=solve(matI)[2,2]
+  
+  vecgammasup[k]=vecgamma[k]+sqrt(varhat)*qnorm(0.95) 
+  
+  vecgammainf[k]=vecgamma[k]+sqrt(varhat)*qnorm(0.05)
+  
+  vecsigmasup[k]=vecsigma[k]+sqrt(varhatsigma)*qnorm(0.95)
+  
+  vecsigmainf[k]=vecsigma[k]+sqrt(varhatsigma)*qnorm(0.05)
+  
+  k=k+1
+  
+}
+
+vecgamma
+
+vecgammacovid=vecgamma
+
+vecgammainfcovid=vecgammainf
+
+vecgammasupcovid=vecgammasup
+
+vecsigmacovid=vecsigma
+
+vecsigmasupcovid=vecsigmasup
+
+vecsigmainfcovid=vecsigmainf
+
+vecucovid=vecu
+
+plot(vecgammacovid~vecucovid,type="l",xlab="Threshold",ylab="xi",main="SARS-CoV-2 clusters in New Jersey",ylim=c(-1,3),xlim=c(0,250),col="red")
+
+points(vecgammainfcovid~vecucovid,type="l",lty=3,col="red")
+
+points(vecgammasupcovid~vecucovid,type="l",lty=3,col="red")
+
+abline(a=0.2,b=0,col="red",lty=5)
+
+abline(v=75,col="blue",lty=5)
+
+tau=0.99
+
+vecquant=1-(1+vecgamma[14]*((1:1000)+1)/vecsigma[14])^(-1/vecgamma[14])
+
+quantau=75+which(vecquant>=tau)[1]
+
+quantau
+
+
+###############################
+
+n=length(Ztot)
+
+vechill=rep(0,n-1)
+
+vecutot=rep(0,n-1)
+
+k=2
+
+while(k<=n){
+  
+  vecutot[k-1]=rev(sort(Ztot))[k]
+  
+  vechill[k-1]=mean(log(Ztot[which(Ztot>=vecutot[k-1])]/vecutot[k-1]))
+  
+  k=k+1
+}
+
+vecgammahill=unique(rev(vechill))
+
+vecuhill=unique(rev(vecutot))
+
+
+i=1
+
+veck=rep(0,length(vecutot))
+
+while(i<=length(vecutot)){
+  
+  veck[i]=which(vecutot==vecutot[i])[1]
+  
+  i=i+1
+}
+
+veck=rev(unique(veck))
+
+
+###################################
+
